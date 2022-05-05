d615ad045273242f9b324bbb3e7e1945df7ba5e8
gauripawar-Atgeir <104908987+gauripawar-Atgeir@users.noreply.github.com>
2022-05-05 14:29:10 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,2 +1,16 @@
 import streamlit
 streamlit.title('My Mom\'s New Healthy Diner')
+
+
+streamlit.header('Breakfast Favorites')
+streamlit.text('🥣Omega 3 & Blueberry Oatmeal')
+streamlit.text('🥗Kale,Spinach & Rocket Smoothie')
+streamlit.text('🐔Hard-Boiled Free-Range Egg')
+streamlit.text('🥑🍞Avocado Toast')
+
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
+
+import streamlit
+streamlit.title('My Mom\'s New Healthy Dinner')
+streamlit.header('Breakfast Favorites')
+streamlit.text('🥣 Omega 3 and Blueberry Oatm
