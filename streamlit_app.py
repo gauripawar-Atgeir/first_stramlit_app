224cd4db04282feae96fcec6293840083f9507d2
gauripawar-Atgeir <104908987+gauripawar-Atgeir@users.noreply.github.com>
2022-05-09 13:28:31 +0530

    Update streamlit_app.py
    add function code

--- streamlit_app.py
+++ streamlit_app.py
@@ -37,20 +37,38 @@ streamlit.dataframe(fruits_to_show)
 #try catch code
 
 #New section to display fruityvice api response
-streamlit.header('Fruityvice fruit Advice!')
-try:
-  fruit_choice = streamlit.text_input('what food would you like information about?')
-  if not fruit_choice:
-    streamlit.error("Please select a fruit to get information.")
-  else:
+#--streamlit.header('Fruityvice fruit Advice!')
+#--try:
+#--  fruit_choice = streamlit.text_input('what food would you like information about?')
+#--  if not fruit_choice:
+#--    streamlit.error("Please select a fruit to get information.")
+#--  else:
+#--    fruityvice_response= requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
+#--    fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+#--    streamlit.dataframe(fruityvice_normalized)
+    
+#--except URLError as e:
+#--  streamlit.error()
+
+#Function code
+#Create the repeatable code block(called a function)
+def get_fruityvice_data(this_fruit_choice):
     fruityvice_response= requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
     fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
-    streamlit.dataframe(fruityvice_normalized)
-    
+    return fruityvice_normalized
+#New section to display fruityvice api response
+streamlit.header('Fruityvice fruit Advice!')
+try:
+    fruit_choice = streamlit.text_input('what food would you like information about?')
+    if not fruit_choice:
+        streamlit.error("Please select a fruit to get information.")
+    else:
+        back_from_function = get_fruityvice_data(fruit_choice)
+        streamlit.dataframe(back_from_function)
 except URLError as e:
-  streamlit.error()
-
-
+    streamlit.error()
+  
+  
 # dont run anything past here while we troubleshott
 streamlit.stop()
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
